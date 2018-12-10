f7ea4983b6cb788ea12c3a9d4b69f34eba1f5ef2
Thiago De Oliveira <tdeoliveira05@gmail.com>
2018-12-10 13:54:25 -0500

    Update master.js

--- master.js
+++ master.js
@@ -5,8 +5,8 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://planswell.lightning.force.com/lightning/o/Task/list*
-// @updateURL    https://github.com/tdeoliveira05/TampermonkeyPP/blob/master/master.js
-// @downloadURL  https://github.com/tdeoliveira05/TampermonkeyPP/blob/master/master.js
+// @updateURL    https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/master.js
+// @downloadURL  https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/master.js
 // @grant        none
 // ==/UserScript==
 
