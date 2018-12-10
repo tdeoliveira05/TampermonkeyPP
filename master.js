c8e4a3c3ffe5b8664badd2d10eaada6708414ab3
Thiago De Oliveira <tdeoliveira05@gmail.com>
2018-12-10 13:29:51 -0500

    Update master.js

--- master.js
+++ master.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Planswell - Salesforce Task UI
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://planswell.lightning.force.com/lightning/o/Task/list*
