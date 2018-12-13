253c876ec11b36fab0462a2360d56d06175053ea
Thiago De Oliveira <tdeoliveira05@gmail.com>
2018-12-13 16:40:41 -0500

    Update master.js

--- master.js
+++ master.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Planswell - Test Script
+// @name         Planswell - TamperMonkey for Plan Pros
 // @namespace    http://tampermonkey.net/
 // @version      2.1
-// @description  try to take over the world!
+// @description  Salesforce General UI Manipulations
 // @author       You
 // @match        https://planswell.lightning.force.com/*
 // @updateURL    https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/master.js
@@ -13,5 +13,39 @@
 (function() {
     'use strict';
 
-    console.log('updated to 2.1');
+    var priorityKeys = {
+        '**': 'Urgent',
+        '!!': 'High'
+    }
+
+    // A function to extract the title of each link (aka "Anchors") in the task list. A list of all titles with priority codes is returned to the function.
+    function matchAllAnchors (priorityKeys) {
+        var anchors = document.getElementsByTagName('a');
+        var anchorArray = [];
+
+        for (var item of anchors) {
+            if (item.title) {
+                var letterArray = item.title.split('');
+                if (letterArray[0] === '*' || letterArray[0] === '!') {
+                    console.log('Priority tasks detected');
+                    anchorArray.push(item)
+                }
+            }
+        }
+
+        return anchorArray;
+    }
+
+    function colorCodeActivate(anchorArray) {
+        anchorArray.forEach((item) => {
+
+            if (item.title.split('')[0] === '*') {item.parentNode.parentNode.parentNode.setAttribute("style", "background-color: #FFC6C6 !important;");}
+            if (item.title.split('')[0] === '!') {item.parentNode.parentNode.parentNode.setAttribute("style", "background-color: #FFE9C6 !important;");}
+        })
+    }
+
+    window.addEventListener("click", function (priorityKeys) {
+        var anchorArray = matchAllAnchors(priorityKeys);
+        colorCodeActivate(anchorArray, priorityKeys);
+    })
 })();
