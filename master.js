c58087bc474026d0f749da8fba624291853d4f8c
Thiago De Oliveira <tdeoliveira05@gmail.com>
2018-12-14 11:46:48 -0500

    Update master.js

--- master.js
+++ master.js
@@ -1,51 +1,92 @@
 // ==UserScript==
 // @name         Planswell - TamperMonkey for Plan Pros
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  Salesforce General UI Manipulations
 // @author       You
 // @match        https://planswell.lightning.force.com/*
 // @updateURL    https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/master.js
 // @downloadURL  https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/master.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var priorityKeys = {
-        '**': 'Urgent',
-        '!!': 'High'
-    }
+    //---------------------------------------------- Task List Color Coding -------------------------------------------//
 
     // A function to extract the title of each link (aka "Anchors") in the task list. A list of all titles with priority codes is returned to the function.
-    function matchAllAnchors (priorityKeys) {
+    function matchAllAnchors (priorityObjs) {
         var anchors = document.getElementsByTagName('a');
         var anchorArray = [];
+        var priorityCodes = [];
+
+        priorityObjs.forEach((item) => {
+            priorityCodes.push(item.code)
+        })
 
         for (var item of anchors) {
             if (item.title) {
                 var letterArray = item.title.split('');
-                if (letterArray[0] === '*' || letterArray[0] === '!') {
-                    console.log('Priority tasks detected');
-                    anchorArray.push(item)
-                }
+                priorityObjs.forEach((obj) => {
+                    if (letterArray[0] === obj.code) {
+                        var anchorObj = {
+                            anchor: item,
+                            meta: obj
+                        }
+                        anchorArray.push(anchorObj)
+                    }
+                })
             }
         }
-
         return anchorArray;
     }
 
+    // A function to execute the color coding by reading an array of information objects contain an anchor element and it's color coding metadata
     function colorCodeActivate(anchorArray) {
-        anchorArray.forEach((item) => {
 
-            if (item.title.split('')[0] === '*') {item.parentNode.parentNode.parentNode.setAttribute("style", "background-color: #FFC6C6 !important;");}
-            if (item.title.split('')[0] === '!') {item.parentNode.parentNode.parentNode.setAttribute("style", "background-color: #FFE9C6 !important;");}
+        anchorArray.forEach((arrayObj) => {
+            var tableRow = arrayObj.anchor.parentNode.parentNode.parentNode
+            tableRow.setAttribute("style", "background-color: " + arrayObj.meta.color + " !important;");
         })
+
     }
 
-    window.addEventListener("click", function (priorityKeys) {
-        var anchorArray = matchAllAnchors(priorityKeys);
-        colorCodeActivate(anchorArray, priorityKeys);
-    })
+    /*
+     *----------------------------------------
+     * The primary executable function to call
+     *----------------------------------------
+     */
+
+    function taskListColorCoding () {
+        var priorityObjs = [
+            {
+                code: "*",
+                description: "Urgent",
+                color: "#FFC6C6"
+            },
+            {
+                code: "!",
+                description: "Important",
+                color: "#FFE9C6"
+            }
+        ]
+
+        var anchorArray = matchAllAnchors(priorityObjs);
+        colorCodeActivate(anchorArray);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------//
+
+    //---------------------------------------- 2-second Userscript Loop ------------------------------------------------//
+
+    setInterval(() => {
+        // Run color coding if user is in the task list view
+        if (document.URL.startsWith('https://planswell.lightning.force.com/lightning/o/Task/list')) {
+            taskListColorCoding();
+        }
+    }, 2000);
+
+
 })();
