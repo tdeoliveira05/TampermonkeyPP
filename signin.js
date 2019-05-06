6f2dc3a7b078f9c38d41ca9a13c5519668ad2ef3
Thiago De Oliveira <tdeoliveira05@gmail.com>
2019-05-06 17:03:32 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Planswell Sign In Form
-// @version      0.1
+// @version      1
 // @description  try to take over the world!
 // @author       You
 // @match        https://docs.google.com/forms/*
+// @updateURL    https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/signin.js
+// @downloadURL  https://raw.githubusercontent.com/tdeoliveira05/TampermonkeyPP/master/signin.js
 // @grant        none
 // ==/UserScript==
 
@@ -11,15 +13,19 @@
     'use strict';
 
     function runTimer () {
+        // Check if the required link element is present
      var tempLink = document.getElementsByClassName("freebirdFormviewerViewResponseLinksContainer")[0].getElementsByTagName("a")[0].href
 
      if (tempLink) {
+         // If element is present, redirect the browser to a new form submit page. Rinse and repeat.
          window.location.href = tempLink;
      }
     }
 
     setInterval ( () => {
+        // Every 5 seconds, check if the user is in a google form
         if (document.URL.startsWith("https://docs.google.com/forms")) {
+            //If true, run the timer for link click
             runTimer()
         }
 
